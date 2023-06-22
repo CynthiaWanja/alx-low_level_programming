451a72780723c2910ebd866413ff520a70916199
CynthiaWanja <cnthwanja@gmail.com>
2023-06-22 12:52:22 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,6 +4,8 @@
  * Description: using _putchar to print
  *
  */
+int _putchar(int c);
+
 void print_alphabet_x10(void)
 {
 char i;
@@ -13,7 +15,7 @@ for (j = 0; j < 10; j++)
 {
 for (i = 'a'; i <= 'z'; i++)
 {
-_putchar(i);
+	_putchar(i);
 }
 _putchar('\n');
 }
